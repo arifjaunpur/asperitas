3d1bdd556af7bc1fbbf835c2a4d6531e6bf59398
root <root@ibm.arifjaunpur.tk>
2019-12-10 10:21:51 +0000

    updated script

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ EOF
 cat > /etc/yum.repos.d/nginx.repo << EOF
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/mainline/centos/7/$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
 gpgcheck=0
 enabled=1
 EOF
